3399665625b40d1605a73c95e56d7e1fbb049dfa
Luca Bobbio <bobbio.luca.117@gmail.com>
2024-01-23 12:01:25 -0300

    Created bike object with name and picture

--- force-app/main/default/lwc/app/app.js
+++ force-app/main/default/lwc/app/app.js
@@ -2,13 +2,16 @@ import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
     ready = false;
+
+    bike = {
+        name: 'Electra X4',
+        picture: 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg'
+    };
     
-    name = 'Electra X4';
     description = 'A sweet bike built for comfort.';
     category = 'Mountain';
     material = 'Steel';
     price = '$2,700';
-    pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
 
     //lifecycle hook
     connectedCallback() {
